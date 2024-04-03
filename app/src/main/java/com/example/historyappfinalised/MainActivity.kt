2e16e1662c1d2c099de39eeee5fafd86b5c1903d
ST10438608 <160500321+ST10438608@users.noreply.github.com>
2024-04-03 15:14:50 +0200

    Variables declared

--- app/src/main/java/com/example/historyappfinalised/MainActivity.kt
+++ app/src/main/java/com/example/historyappfinalised/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.historyappfinalised
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +14,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+
+
+        val user_age = findViewById<EditText>(R.id.editTextNumber)
+        val historical_data = findViewById<TextView>(R.id.textView2)
+        val generate = findViewById<Button>(R.id.button)
+        val clear = findViewById<Button>(R.id.button2)
         }
     }
 }
